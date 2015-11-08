d5f139aff834b6704cb8435b8467aa94cdc44f40
jgb93 <jgb93@ougl-17-g.eplt.washington.edu>
2015-11-08 15:08:48 -0800

    added a pause button, and styled the popup

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){
-  $('#filterBar').hide();
-  $('#popup').hide();
+  $('#filterBar').hide(); // start with filter bars hidden
+  $('#popup, .overlay').hide(); //start with popup hidden
 
-  $('#popup').click(function() {
-    $('#popup').hide();
+  // hide popup if the popup is clicked
+  $('#popup, .overlay').click(function() {
+    $('#popup, .overlay').hide();
   });
 
   // $('[data-toggle="tooltip"]').tooltip(); 
 
-  $('img').tooltip({title: "Hooray!"});
+  $('.playingSpan').tooltip({title: "Hooray!"});
 
 });
 
 var data;
 var baseUrl = 'https://api.spotify.com/v1/search?type=track&query=artist:'
 var myApp = angular.module('myApp', []);
-var scopeArtist;
+var currentArtist;
 var trackName;
 
 var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
@@ -23,40 +24,28 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
 
   // get songs by artist search
   $scope.getSongs = function() {
-    console.log("the scopeArtist is " + scopeArtist);
-    console.log("the artist is " + $scope.artist);
-    console.log("the track is " + $scope.track);
-    console.log("the album is " + $scope.album);
-
-    if ($scope.artist != scopeArtist && $scope.track == undefined && $scope.album == undefined) {
-      scopeArtist = $scope.artist;
-      console.log("new scopeArtist is " + scopeArtist);
+    if ($scope.artist != currentArtist && $scope.track == undefined && $scope.album == undefined) {
+      currentArtist = $scope.artist;
       $('#filterBar').show();
-      console.log(baseUrl);
       $http.get(baseUrl + $scope.artist).success(function(response){ 
-        console.log(baseUrl + $scope.artist);
         data = $scope.tracks = response.tracks.items;  
       });
-    } else if ($scope.artist == scopeArtist && $scope.track == undefined && $scope.album == undefined) {
+    } else if ($scope.artist == currentArtist && $scope.track == undefined && $scope.album == undefined) {
         $http.get('https://api.spotify.com/v1/search?type=track&query=artist:' + $scope.artist).success(function(response){ 
-          console.log(baseUrl + $scope.artist);
           data = $scope.tracks = response.tracks.items;  
         });
     }
 
     // if user searchs a track or an album or both
     if ($scope.track != undefined &&$scope.album == undefined) {
-      console.log(baseUrl + $scope.artist + "%20track:" + $scope.track);
       $http.get(baseUrl + $scope.artist + "%20track:" + $scope.track).success(function(response){ 
         data = $scope.tracks = response.tracks.items;  
       });
     } else if ($scope.track == undefined && $scope.album != undefined) {
-      console.log(baseUrl + $scope.artist + "%20album:" + $scope.album);
       $http.get(baseUrl + $scope.artist + "%20album:" + $scope.album).success(function(response){ 
         data = $scope.tracks = response.tracks.items;  
       });
     } else if ($scope.track != undefined && $scope.album != undefined) {
-      console.log(baseUrl + $scope.artist + "%20track:" + $scope.track + "%20album:" + $scope.album);
       $http.get(baseUrl + $scope.artist + "%20track:" + $scope.track + "%20album:" + $scope.album).success(function(response){ 
         data = $scope.tracks = response.tracks.items;  
       });
@@ -66,22 +55,34 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
 
   // play the preview_url
   $scope.play = function(song, track) {
-    if($scope.currentSong == song) { //if the current song url is the same as the the one playing
-      $scope.audioObject.pause(); //pause the song
-      $scope.currentSong = false; //make the current song false
-      $('#popup').hide();
+    if($scope.currentSong == song) { 
+      $scope.audioObject.pause(); 
+      $scope.currentSong = false; 
       return;
-    } else { //if the current song url isn't the same as the one playing
-      if($scope.audioObject.pause != undefined) { //if the song isn't paused
-        $scope.audioObject.pause() // pause that song
-        $('#popup').hide();
+    } else { 
+      if($scope.audioObject.pause != undefined) { 
+        $scope.audioObject.pause() 
       }
-      $scope.audioObject = new Audio(song); //then create a new audio object with the new url
-      $scope.audioObject.play();   //play that song
-      $scope.currentSong = song; //change the song from false to the current url
-      $('#popup').show();
+      $scope.audioObject = new Audio(song);
+      $scope.audioObject.play();   
+      $scope.currentSong = song; 
+      $('#popup, .overlay').show();
       $scope.trackName = track.name;
-      $('#popup').html("<img src='" + track.album.images[1].url + "' alt='" + track.name + "' title='" + track.name + "'>");
+
+      // clear the popup, then append information
+      $('#popup').html("");
+      $('#popup').append("<img width=" + 450 + " height=" + 450 + " src=" + track.album.images[0].url + " alt=" + track.name + ">");
+      $('#popup').append('<h4>"' + track.name + '" by ' + track.artists[0].name + "</h4>")
+      $('#popup').append("<h4>" + "from the album <i>" + track.album.name + "</i></h4>")
+
+    }
+  }
+
+  $scope.pause = function() {
+    if(!$scope.audioObject.paused) {
+      $scope.audioObject.pause();
+      $scope.currentSong = false;
+      return;
     }
   }
 });
