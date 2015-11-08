9cf93f956f219a082e71eb8aadf0fb7322c0cc6a
Julian Bossiere <julianbossiere@gmail.com>
2015-11-07 23:42:55 -0800

    snapshot

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
   $('#filterBar').hide();
   $('#popup').hide();
+
+  $('#popup').click(function() {
+    $('#popup').hide();
+  });
+
 });
 
 var data;
@@ -10,7 +15,6 @@ var scopeArtist; // used this for debugging - delete at the end
 var trackName;
 
 var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
-  // $('#animateDiv').hide();
   $scope.audioObject = {}
 
   // get songs by artist search
@@ -36,7 +40,6 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
         });
     }
 
-
     // if user searchs a track or an album or both
     if ($scope.track != undefined &&$scope.album == undefined) {
       console.log(baseUrl + $scope.artist + "%20track:" + $scope.track);
@@ -54,50 +57,33 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
         data = $scope.tracks = response.tracks.items;  
       });
     } 
-  };
+  }
   
-  // get artists
-  $scope.getArtist = function() {
-    $http.get(artistUrl + $scope.artist).success(function(response){ 
-      data = $scope.artists = response.artists.items;
-      console.log('hello1');
-      console.log('hello2');
-      console.log(data);
-      //$scope.artist = '';  erases user input after submit
-    });
-  };
 
   // play the preview_url
   $scope.play = function(song, track) {
-    if($scope.currentSong == song) {
-      $scope.audioObject.pause();
-      $scope.currentSong = false;
+    if($scope.currentSong == song) { //if the current song url is the same as the the one playing
+      $scope.audioObject.pause(); //pause the song
+      $scope.currentSong = false; //make the current song false
+      $('#popup').hide();
       return;
-    } else {
-      if($scope.audioObject.pause != undefined) $scope.audioObject.pause()
-      $scope.audioObject = new Audio(song);
-      $scope.audioObject.play();  
-      $scope.currentSong = song;
+    } else { //if the current song url isn't the same as the one playing
+      if($scope.audioObject.pause != undefined) { //if the song isn't paused
+        $scope.audioObject.pause() // pause that song
+        $('#popup').hide();
+      }
+      $scope.audioObject = new Audio(song); //then create a new audio object with the new url
+      $scope.audioObject.play();   //play that song
+      $scope.currentSong = song; //change the song from false to the current url
+      $('#popup').show();
+      trackName = track.name;
+      $scope.trackName = trackName;
+      $('#popup').html("<img src='" + track.album.images[1].url + "' alt='" + track.name + "' title='" + track.name + "'>");
     }
-
-    // gets track name in order to show current playing track
-    trackName = track.name;
-    $scope.trackName = trackName;
-    popup(track);
-  };
-
-  var popup = function (track) {
-    console.log('kk');
-    console.log(trackName)
-    var trackImage = track.album.images[1].url;
-    console.log(trackImage);
-    $('#popup').show();
-    $('#popup').html("<img src=https://i.scdn.co/image/9f31af5ca97f84f23a46ad152a89481668e6008f alt='trackName' title='trackName'>"); //Why isn't this thing working? if I put any other image url, it shows!
-  }; 
-//  Problem displaying image could be caused by response format (https://developer.spotify.com/technologies/metadata-api/) Figure out how to use?
-// Try putting a different HTML element to send to the popup and see if it shows up on the page.
-
+  }
 });
+
+
 
 // Add tool tips to anything with a title property
 $('body').tooltip({
