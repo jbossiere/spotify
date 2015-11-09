bbee8858b1764eb49319d0fe0993398be7d24c5e
Julian Bossiere <julianbossiere@gmail.com>
2015-11-09 00:06:43 -0800

    can only escape popup if outside of the popup window is clicked.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   $('#popup, .overlay').hide(); //start with popup hidden
 
   // hide popup if the popup is clicked
-  $('#popup, .overlay').click(function() {
+  $('.overlay').click(function() {
     $('#popup, .overlay').hide();
   });
 
